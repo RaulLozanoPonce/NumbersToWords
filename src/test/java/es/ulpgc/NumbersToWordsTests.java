ae930ca98ac8dc22c4d42f1175619f20e4e5111e
RaulLozanoPonce <raul.lozano.ponce98@gmail.com>
2019-09-23 17:40:22 +0200

    Pass the first test

---
+++ src/test/java/es/ulpgc/NumbersToWordsTests.java
@@ -0,0 +1,35 @@
+package es.ulpgc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(org.junit.runners.Parameterized.class)
+public class NumbersToWordsTests {
+
+    private int number;
+    private String numberWord;
+
+    public NumbersToWordsTests(int number, String numberWord) {
+        this.number = number;
+        this.numberWord = numberWord;
+    }
+
+    @Test
+    public void execute(){
+        assertThat(toWord(number)).isEqualTo(this.numberWord);
+    }
+
+    private String toWord(int number) {
+        return "twenty";
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases() {
+        return new Object[][]{
+                {20, "twenty"}
+        };
+    }
+
+}
